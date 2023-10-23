716ba9736cc91cb13def2912023cf3cb537025b4
Adolfo Angel Salomé Hernández <al222010588@gmail.com>
2023-10-23 00:18:41 -0600

    Configuraciones para MAIL

--- src/config.js
+++ src/config.js
@@ -9,3 +9,8 @@ export default {
 export const URL_CONNECTION = process.env.URL_CONNECTION
 export const API_PORT = process.env.API_PORT || 3007
 export const COOKIE_SECRET = process.env.COOKIE_SECRET
+
+export const EMAIL_HOST = process.env.EMAIL_HOST
+export const EMAIL_PORT = process.env.EMAIL_PORT
+export const EMAIL_USER = process.env.EMAIL_USER
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
